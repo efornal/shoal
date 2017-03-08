671bf51cf4ed580f328d26b5ea5199b905f34c33
esteban <efornal@gmail.com>
2017-03-08 11:01:34 -0300

    language configuration

--- app/views.py
+++ app/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.utils.translation import ugettext as _
+from .models import LdapConn
+from django.shortcuts import redirect
+from django.utils import translation
+import logging
 
-# Create your views here.
-@login_required
+
 def index(request):
     context={}
+    people = LdapConn.people_by_uid('ef')
+    context.update({'people': people})
+    return render(request, 'index.html', context)
+
+
+def search(request):
+    context={}
+
     return render(request, 'index.html', context)
