9826786cdaa354a11e5dc51c8d4467d77d982539
esteban <efornal@gmail.com>
2017-03-17 13:02:26 -0300

    Initials proofs for model use without database

--- shoal/urls.py
+++ shoal/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.conf.urls.i18n import i18n_patterns
 from django.utils.translation import ugettext_lazy as _
 admin.autodiscover()
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('app.urls')),
