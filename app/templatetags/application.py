75c4e1a38e96de06e733e0d6f3dea546b6445328
esteban <efornal@gmail.com>
2017-03-29 09:53:26 -0300

    Fixed menu bar. Added application title

--- app/templatetags/application.py
+++ app/templatetags/application.py
@@ -4,9 +4,10 @@ from django.conf import settings
 
 register = template.Library()
 
-
+import logging
 @register.filter
 def application_title(value):
+    logging.error(settings.APPLICATION_NAME)
     title = ''
     if value:
         title = value
