ef25458321937dd2954ddf4276ab99409c7b46a6
Esteban Fornal <efornal@rectorado.unl.edu.ar>
2019-11-28 13:03:22 -0300

    bug fix

--- app/http_auth.py
+++ app/http_auth.py
@@ -10,7 +10,6 @@ def basic_http_authentication(request):
     if len(auth) == 2:
         if auth[0].lower() == "basic":
             uname, passwd = base64.b64decode(auth[1]).split(':')
-            logging.error("{} {}".format(uname, passwd))
             user = authenticate(username=uname, password=passwd)
     return user
 
