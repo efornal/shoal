bf5e8adc48b1b111ecc44d30d75e48b5b2641c1d
Esteban <efornal@gmail.com>
2023-07-27 08:17:20 -0300

    run migrations only if variable is defined

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 set -e
+echo "Status SKIP_APP_INIT: $SKIP_APP_INIT"
 
-python manage.py compilemessages
-python manage.py collectstatic --noinput
-python manage.py migrate --database db_owner
-
+if [ -z "$SKIP_APP_INIT" ]; then
+    echo "Running application initialization scripts.."
+    python manage.py compilemessages
+    python manage.py collectstatic --noinput
+    python manage.py migrate --database db_owner
+fi
+    
 exec "$@"
+
+
+
